430e5b5cd87fc6fba5bf9f36cb799c3a47529b1a
francoode <francoode@gmail.com>
2024-12-11 23:50:29 -0300

    init modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MicroserviceOptions } from '@nestjs/microservices/interfaces';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://user:password@rabbitmq:5672'],
+      queue: 'logs_queue',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
